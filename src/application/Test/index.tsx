ad82a83cba28b469a00cf8012bdf3253c332b88c
Shukur Aliev <abdushukur.aliev@haiku.dev>
2022-10-19 19:27:30 +0500

    fix: add test loadable component

---
+++ src/application/Test/index.tsx
@@ -0,0 +1,9 @@
+import styles from './styles.module.scss';
+
+const Test = () => (
+  <div className={styles.test}>
+    TEST
+  </div>
+)
+
+export default Test
--- src/application/index.tsx
+++ src/application/index.tsx
@@ -1,5 +1,13 @@
+import loadable from '@loadable/component';
 import { Input } from '../Input';
 
+const LazyTest = loadable(() => import('./Test'));
+
 export const App = () => (
-  <div><Input/></div>
+  <div>
+    <Input/>
+    {false && (
+      <LazyTest/>
+    )}
+  </div>
 )
